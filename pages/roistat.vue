4355e5879ab292cb6a19761461e89cb894cbbb39
Yevhen Lisovenko <yevhenlisovenko@gmail.com>
2021-12-13 17:04:34 +0100

    fix: added roistat for testing

---
+++ pages/roistat.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1 class="uk-heading-small">Roistat Test</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RoistatPage',
+    head() {
+        return {
+            script: [
+                {
+                    innerHTML: `
+                    window.onRoistatAllModulesLoaded = function () {
+    window.roistat.registerOnVisitProcessedCallback(function() {
+        console.log("Установлен номер визита: " + window.roistat.getVisit());
+        new window.EasyWeekWidget({
+            url: "https://widget.easyweek.io/awescode-gmbh?roistat_visit=" + window.roistat.getVisit(),
+            button: { text: 'Онлайн-запись', showText: true }
+        });
+    });
+};`,
+                    type: 'text/javascript',
+                    body: true
+                }
+            ],
+            // this line need for application/ld+json
+            __dangerouslyDisableSanitizers: ['script']
+        }
+    }
+}
+</script>
